f58b79c6de25628dde40bf8f4c16b6653d6d9005
Rasmus Hansson <rasmusfredrikhansson@gmail.com>
2021-03-12 13:44:48 +0100

    Close calendar code implemented

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -15,10 +15,9 @@ if (props.show) {
 
   return (
       <div className={contactFormClassses}>
-          <h2>We will be cleaning your home on the</h2>
+          <h2>We will be cleaning your home on the : insert time and date here : 28/3/2021 in the morning</h2>
           <form action="">
-            <input className="input-box" type="text" placeholder="First name"></input>
-            <input className="input-box" type="text" placeholder="Last name"></input>
+            <input className="input-box" type="text" placeholder="Full name"></input>            
             <input className="input-box" type="tel" placeholder="Telephone"></input>
             <input className="input-box" type="email" placeholder="Email address"></input>
             <input className="input-box" type="text" placeholder="Address 1"></input>
@@ -26,8 +25,7 @@ if (props.show) {
             <button type="submit" className="send-form">Book Now</button>         
           </form>       
           <button className="send-form" onClick={bookSuccessfully}>Book Now</button> 
-        </div>
-    
+        </div>    
   )
 }
 
--- src/components/TheCalendar.js
+++ src/components/TheCalendar.js
@@ -26,6 +26,7 @@ const TheCalendar = props => {
 
   const openForm = () => {
     setFormOpen(open => !open)  
+    setCalendarClose(true)
   }
 
   const changeTime = (value) => {
@@ -33,26 +34,29 @@ const TheCalendar = props => {
   }
 
   return (
-    <div className={calendarClasses}>
-      <Calendar
-        onChange={onCalendarChange} 
-        value={date} 
-        showNeighboringMonth={true}     
-      />      
+    <div>
+      <div className={calendarClasses}>
+        <Calendar
+          onChange={onCalendarChange} 
+          value={date} 
+          showNeighboringMonth={true}     
+        />      
 
-    
+      
 
-      <div className="radio-btn">
-        <input onChange={ changeTime } className="radio__input" type="radio" name="time" id="morning" value="morning"></input>
-        <label className="radio__label" htmlFor="morning">Morning</label>
-        <input onChange={ changeTime } className="radio__input" type="radio" name="time" id="afternoon" value="afternoon"></input>
-        <label className="radio__label" htmlFor="afternoon">Afternoon</label>
+        <div className="radio-btn">
+          <input onChange={ changeTime } className="radio__input" type="radio" name="time" id="morning" value="morning"></input>
+          <label className="radio__label" htmlFor="morning">Morning</label>
+          <input onChange={ changeTime } className="radio__input" type="radio" name="time" id="afternoon" value="afternoon"></input>
+          <label className="radio__label" htmlFor="afternoon">Afternoon</label>
+        </div>
+
+        <div> 
+          <button className="bookNow" onClick={ openForm }>Book now</button>
+        </div>
+        
       </div>
-
-      <div> 
-        <button className="bookNow" onClick={ openForm }>Book now</button>
-      </div>
-      <ContactForm show={formOpen} contactDate={date} />
+    <ContactForm show={formOpen} contactDate={date} />
     </div>
   )
 }
