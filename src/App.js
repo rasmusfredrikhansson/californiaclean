2d299f73a40ed8f6e55a884a6ac362c947227749
Rasmus Hansson <rasmusfredrikhansson@gmail.com>
2021-03-18 19:14:43 +0100

    Removed Facebook link, cannot use on mobile

--- src/App.js
+++ src/App.js
@@ -257,7 +257,7 @@ const App = () => {
 
     <div className="socialMedia" data-aos="fade-right">
       <p>{t('Follow.1')}</p>
-      <a href="https://www.facebook.com/californiaclean75" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
+      {/* <a href="https://www.facebook.com/californiaclean75" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a> */}
       <a href="https://www.instagram.com/californiaclean.instockholm/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram-square"></i></a>
     </div>
       
