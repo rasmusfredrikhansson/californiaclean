c58a1c0978ab5b4f1a3e200d87003d40b29f553b
Rasmus Hansson <rasmusfredrikhansson@gmail.com>
2021-04-05 14:16:26 +0200

    Added email form

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,5 +1,20 @@
 import React from 'react'
 import './Email.css'
+import emailjs from 'emailjs-com'
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  
+  emailjs.sendForm('service_w881ah5', 'template_oxrg84a', e.target, 'user_IvPDcCwFV9Mmqlfzd7OSD')
+
+    .then((result) => {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text);
+    });
+  e.target.reset()
+}
+
 
 const Email = () => {
   return (
@@ -7,14 +22,17 @@ const Email = () => {
       <div className="email_message">
         Ring upp mig
       </div>
-      <input type="text" placeholder="Namn"></input>
-      <input type="text" placeholder="Telefonnummer"></input>      
-      <select>
-        <option value="Engångsstädning">Engångsstädning</option>
-      </select>
-      <br/>
-      {/* <textarea rows="4" placeholder="Meddelande"></textarea> */}
-      <button type="submit">Skicka meddelande</button>
+      <form onSubmit={sendEmail}>
+        <input type="text" placeholder="Namn" name="name"></input>
+        <input type="text" placeholder="Telefonnummer" name="telephone"></input>      
+        <select name="service">
+          <option value="Engångsstädning">Engångsstädning</option>
+          <option value="Engångsstädning">Regelbunden städning</option>
+        </select>
+        <br/>
+        {/* <textarea rows="4" placeholder="Meddelande"></textarea> */}
+        <button type="submit">Skicka meddelande</button>
+      </form>
     </div>
   )
 }
