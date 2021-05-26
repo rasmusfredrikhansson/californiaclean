df185a05fdfede9f4cc184d54075101db7f16728
Rasmus Hansson <rasmusfredrikhansson@gmail.com>
2021-05-26 15:30:24 +0200

    Style fixes

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -49,7 +49,16 @@ const ContactForm = (props) => {
           <input className="input-box" type="hidden" name="date" value={props.contactDate} />
           <input className="input-box" type="hidden" name="time" value={props.contactTime} />
           <input className="input-box" type="tel" placeholder={t('Booking.5')} name="telephone" required="required" />
-          <input className="input-box" type="number" placeholder={t('Booking.6')} name="squaremeters" required="required" />
+          <input className="input-box" type="number" placeholder={t('Booking.6')} name="squaremeters" required="required" />          
+            <select placeholder="choose service" name="services">
+              <option value={t('Booking.9')}>{t('Booking.9')}</option>
+              <option value={t('Booking.10')}>{t('Booking.10')}</option>
+              <option value={t('Booking.11')}>{t('Booking.11')}</option>
+              <option value={t('Booking.12')}>{t('Booking.12')}</option>
+              <option value={t('Booking.13')}>{t('Booking.13')}</option>
+              <option value={t('Booking.14')}>{t('Booking.14')}</option>
+              <option value={t('Booking.15')}>{t('Booking.15')}</option>             
+            </select>
           <input className="input-box" type="text" placeholder={t('Booking.7')} name="adress" required="required" />
           <input className="input-box" type="text" placeholder={t('Booking.8')} name="postadress" required="required" />
           <button className="send-form" type="submit">{t('Booking.3')}</button>
